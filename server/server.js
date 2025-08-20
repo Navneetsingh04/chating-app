8157e5d5e52f67d0dd5f047a7c464eac8cd37419
Navneet Singh <navneetsingh1825@gmail.com>
2025-08-21 03:23:33 +0530

    fix: error in Server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 
 import { connectDB } from "./config/db.js";
 
@@ -10,26 +11,37 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true,
-}))
+const __dirname = path.resolve();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 
-import authRoutes from "./routes/auth.route.js"
-import userRoutes from "./routes/user.route.js"
-import chatRoutes from "./routes/chat.route.js"
+import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
+import chatRoutes from "./routes/chat.route.js";
 
 app.get("/", (req, res) => {
   res.send("Server is Running");
 });
 
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/chat", chatRoutes);
 
-app.use("/api/auth",authRoutes);
-app.use("/api/users",userRoutes);
-app.use("/api/chart",chatRoutes);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server is Running on Port ${PORT}`);
