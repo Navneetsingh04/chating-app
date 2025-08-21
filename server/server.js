63435a9510a639d9360384b20658d9fc86c9beb9
Navneet Singh <navneetsingh1825@gmail.com>
2025-08-21 13:11:47 +0530

    fix: error in server.js

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import path from "path";
 
 import { connectDB } from "./config/db.js";
 
@@ -10,8 +9,6 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
-
-const __dirname = path.resolve();
 
 app.use(
   cors({
@@ -34,14 +31,6 @@ app.get("/", (req, res) => {
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/dist")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
-  });
-}
 
 app.listen(PORT, () => {
   console.log(`Server is Running on Port ${PORT}`);
